1ae1a964404d701806bfda74625c3ad8189c5ef6
Anna Ivanova <anivse@users.noreply.github.com>
2021-10-12 01:10:10 +0300

    Revert "Update lab.c"
    This reverts commit f3dab9f86b2231952329fbd1d4b2acf55603c920.

--- alg_lab_1/alg_lab_1/lab.c
+++ alg_lab_1/alg_lab_1/lab.c
@@ -24,15 +24,34 @@ void DestroyList(list_t* list) {
 	}
 }
 
-  int NumberMonth(char* month) {
-	char* arr[] = { "January","February","March","April","May","June","July","August","September","October","November","December" };
-	for (int i = 1;i <= 12;i++) {
-		if (strcmp(month, arr[i-1]) == 0) {
-			return i;
-		}
-	}
-	return 0;
-  }
+int NumberMonth(char* month) {
+	if (strcmp(month, "January") == 0)
+		return 1;
+	else if (strcmp(month, "February") == 0)
+		return 2;
+	else if (strcmp(month, "March") == 0)
+		return 3;
+	else if (strcmp(month, "April") == 0)
+		return 4;
+	else if (strcmp(month, "May") == 0)
+		return 5;
+	else if (strcmp(month, "June") == 0)
+		return 6;
+	else if (strcmp(month, "July") == 0)
+		return 7;
+	else if (strcmp(month, "August") == 0)
+		return 8;
+	else if (strcmp(month, "September") == 0)
+		return 9;
+	else if (strcmp(month, "October") == 0)
+		return 10;
+	else if (strcmp(month, "November") == 0)
+		return 11;
+	else if (strcmp(month, "December") == 0)
+		return 12;
+	else
+		return 0;
+}
  
 int ItsName(char* fullName) {
 	int length = strlen(fullName);
