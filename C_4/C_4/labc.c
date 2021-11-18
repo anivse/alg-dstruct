6ec64c78542048fa5061560b399ba4a434b2e579
Anna Ivanova <anivse@users.noreply.github.com>
2021-11-19 01:13:18 +0300

    Create files

---
+++ C_4/C_4/labc.c
@@ -0,0 +1,248 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+#include "labc.h"
+
+#pragma warning (disable:4996)
+#pragma warning (disable:6031)
+
+queue_t* QueueCreate(int value)
+{
+	queue_t* queue = (queue_t*)malloc(sizeof(queue_t));
+	if (!queue)
+	{
+		perror("Memory error");
+		return NULL;
+	}
+	queue->head = (list_t*)malloc(sizeof(list_t));
+	if (!queue->head)
+	{
+		perror("Memory error");
+		return NULL;
+	}
+	queue->head->field = value;
+	queue->head->next = NULL;
+	queue->tail = queue->head;
+	return queue;
+}
+
+void AddInQueue(queue_t* queue, int field)
+{
+	if (!queue->tail)
+	{
+		QueueCreate(field);
+		return;
+	}
+	queue->tail->next = (list_t*)malloc(sizeof(list_t));
+	if (!queue->tail->next)
+	{
+		perror("Memory error");
+		return;
+	}
+	queue->tail = queue->tail->next;
+	queue->tail->field = field;
+	queue->tail->next = NULL;
+}
+
+void DeleteOutQueue(queue_t* queue)
+{
+	if (queue)
+	{
+		list_t* temp;
+		temp = queue->head;
+		queue->head = queue->head->next;
+		free(temp);
+	}
+}
+
+graph_t GraphCreate()
+{
+	FILE* file = fopen("test.txt", "r");
+	graph_t graph;
+	graph_t error = { 0,NULL };
+	edge_t* tmp;
+	fscanf(file, "%i", &graph.size);
+	graph.edge = malloc(sizeof(edge_t));
+	if (!graph.edge)
+	{
+		perror("Memory error");
+		return error;
+	}
+	int i = 0;
+	while ((fscanf(file, "%i", &graph.edge[i].one) != EOF) && (fscanf(file, "%i", &graph.edge[i].two) != EOF))
+	{
+		i++;
+		tmp = realloc(graph.edge, (i + 1) * sizeof(edge_t));
+		if (tmp != NULL)
+		{
+			graph.edge = tmp;
+		}
+	}
+	graph.edge[i].one = 0;
+	graph.edge[i].two = 0;
+	fclose(file);
+	return graph;
+}
+
+void ShirtArray(edge_t edge[], int number)
+{
+	int i = number;
+	while ((edge[i].one != 0) || (edge[i].two != 0))
+	{
+		edge[i] = edge[i + 1];
+		i++;
+	}
+}
+
+int FoundNextVert(graph_t graph, int top)
+{
+	int i = 0;
+	int nextVert = graph.size, count;
+	while ((graph.edge[i].one != 0) || (graph.edge[i].two != 0))
+	{
+		if (graph.edge[i].one == top)
+		{
+			if (graph.edge[i].two < nextVert)
+			{
+				nextVert = graph.edge[i].two;
+				count = i;
+			}
+		}
+		if (graph.edge[i].two == top)
+		{
+			if (graph.edge[i].one < nextVert)
+			{
+				nextVert = graph.edge[i].one;
+				count = i;
+			}
+		}
+		i++;
+	}
+	if (nextVert != graph.size)
+	{
+		ShirtArray(graph.edge, count);
+		return nextVert;
+	}
+	return 0;
+}
+
+int IsInQuene(queue_t* queue, int value)
+{
+	list_t* tmp = queue->head;
+	while (tmp != NULL)
+	{
+		if (tmp->field == value)
+		{
+			return 1;
+		}
+		tmp = tmp->next;
+	}
+	return 0;
+}
+
+int IsInArr(int* arr, int size, int value)
+{
+	for (int i = 0; i < size; i++) {
+		if (arr[i] == value) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void QueueDestroy(queue_t* queue)
+{
+	list_t* delete = NULL;
+	while (queue->head != NULL) {
+		delete = queue->head;
+		queue->head = queue->head->next;
+		free(delete);
+	}
+}
+
+void Print(int* arr, int size)
+{
+	for (int i = 0;i < size;i++)
+	{
+		printf("%i ", arr[i]);
+	}
+}
+
+void GraphBFS()
+{
+	graph_t graph = GraphCreate();
+	queue_t* queue = QueueCreate(0);
+	if (!queue)
+	{
+		return;
+	}
+	int* BFS = (int*)malloc(graph.size * sizeof(int));
+	if (!BFS)
+	{
+		perror("Memory Error");
+		return;
+	}
+	int i = 0;
+	int countBFS = 0;
+	while (countBFS < graph.size)
+	{
+		if (queue->head == NULL)
+			break;
+		int tmp = queue->head->field;
+		int vert = FoundNextVert(graph, tmp);
+		while (vert != 0)
+		{
+			if ((IsInQuene(queue, vert) == 0) && (IsInArr(BFS, countBFS + 1, vert) == 0))
+			{
+				AddInQueue(queue, vert);
+			}
+			vert = FoundNextVert(graph, tmp);
+		}
+		DeleteOutQueue(queue);
+		BFS[countBFS] = tmp;
+		countBFS++;
+	}
+	QueueDestroy(queue);
+	Print(BFS, countBFS);
+	free(BFS);
+	free(graph.edge);
+}
+
+int Random(int left, int right)
+{
+	int k;
+	double value;
+	k = rand();
+	value = ((double)k) / (RAND_MAX + 1);
+	value = value * (right - left) + left;
+	return (int)value;
+}
+
+void StressTest()
+{
+
+	LARGE_INTEGER start, end, counter;  
+	double time;
+
+	int numVert = 100000;
+
+	FILE* file = fopen("test.txt", "w");
+	fprintf(file,"%i\n", numVert);
+
+	for (int i = 0;i < numVert * 2;i++)
+	{
+		fprintf(file, "%i  %i\n", Random(0, 99999), Random(0, 99999));
+	}
+
+	fclose(file);
+
+	QueryPerformanceFrequency(&counter);
+	QueryPerformanceCounter(&start);
+
+	GraphBFS();
+
+	QueryPerformanceCounter(&end);
+	time = (double)((double)(end.QuadPart - start.QuadPart) / (double)counter.QuadPart);
+
+	printf("%lf", time);
+}
---
+++ C_4/main/main.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"labc.h"
+
+/**
+*
+* CPU: AMD Ryzen 3 3200U with Radeon Vega Mobile Gfx 2.60 GHz
+* RAM: 4 GB
+* SSD: 234 GB
+*
+* Vertex number 100000
+* 
+* Result - 39 seconds
+*
+*/
+
+
+int main()
+{
+	StressTest();
+}
